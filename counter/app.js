004e1f1e3ef295749b692bed020886c85c64c8ec
Renato Boucas <renatoboucas@gmail.com>
2022-01-04 10:01:15 -0500

    Counter - correcting reset counter color

--- counter/app.js
+++ counter/app.js
@@ -17,7 +17,16 @@ btns.forEach(function (btn) {
         else {
             count = 0;
         }
-
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        if (count < 0) {
+            value.style.color = 'red'; 
+        }
+        if (count === 0) {
+            value.style.color = '#222';
+        }
+        
         value.textContent = count;
 
     })
